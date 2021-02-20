b883ea2854c850ed9ea513c7dabac108a8650582
rzjprogramador <rzjprogramador@gmail.com>
2021-02-20 02:13:51 -0300

    NUmeros Inteiro e Decimais

---
+++ a__Numeros.py
@@ -0,0 +1,22 @@
+# ============ INTEIRO ============
+
+# no Console :: python3 >> saber o tipo :: type(<o que quer saber o tipo>)
+
+# modulo resto da divisão :: 5 % 2 resposta 1
+
+# divisao float  :: 5 / 2 :: resposta 2.5
+
+# divisão inteira sem sobra :: 5 // 2  :: resposta 2
+
+# exponenciação :: 2 ** 3  (2 elevado a 3) :: resposta 8
+
+# Numeros legiveis :casas decimais separadas por underscore _ anderline
+# Um milhão == 1_000_000
+
+# ===================================================
+
+# ============ FLOAT -- NUMEROS DECIMAIS ============
+
+# Converter inteiro em float :: int(<NumeroFloat)  -- ex: int(1.5)
+
+# Converter float em inteiro :: float(<NumeroInteiro>) -- ex: float(1)
