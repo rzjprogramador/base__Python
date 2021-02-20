ad0d8d864801a797bd862da54ed843fb67f4f486
rzjprogramador <rzjprogramador@gmail.com>
2021-02-20 02:56:26 -0300

    Condicionais if - elif - else

---
+++ c__Condicionais.py
@@ -0,0 +1,16 @@
+idade = 16
+
+if idade < 18:
+    print('Menor de Idade')
+elif idade < 60:
+    print('Melhor Idade')
+else:
+    print('Invalido')
+
+
+# Escopo ::: Escopo começa em 2 ponto : e termina na ultima linha vazia sem nada.
+
+# Identação super importante pra rodar o codigo
+# if == Se
+# elif == senao se (Posso repetir quanto quiser esta opção sempre dara noav possibilidade)
+# else == senão >>> ultima opção do if
