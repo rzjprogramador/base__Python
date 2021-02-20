a5c094300de1cc7138d04ea2b2074e895b395343
rzjprogramador <rzjprogramador@gmail.com>
2021-02-20 13:46:56 -0300

    Numericos

--- a__Numericos.py
+++ a__Numericos.py
@@ -1,22 +1,39 @@
+# ============ ORDENS ============
+
+# Ordem Prescedencia  Expressões ::
+# 1º -- () -- tudo que esta dentro dos parenteses
+# 2º -- ** -- exponenciação
+# 3º -- *  -- multiplicação
+# 4º -- /  -- divisão
+# Penultimo :: + soma
+# Ultimo ::   - subtração
+
+
 # ============ INTEIRO ============
 
-# no Console :: python3 >> saber o tipo :: type(<o que quer saber o tipo>)
-
 # modulo resto da divisão :: 5 % 2 resposta 1
+# (Porque 5 divide por 2 da 2 -- e sobra 1 --esta sobra é o resto da divisão)
+
 
 # divisao float  :: 5 / 2 :: resposta 2.5
 
+
 # divisão inteira sem sobra :: 5 // 2  :: resposta 2
 
+
 # exponenciação :: 2 ** 3  (2 elevado a 3) :: resposta 8
+
 
 # Numeros legiveis :casas decimais separadas por underscore _ anderline
 # Um milhão == 1_000_000
 
+
 # ===================================================
+
 
 # ============ FLOAT -- NUMEROS DECIMAIS ============
 
 # Converter inteiro em float :: int(<NumeroFloat)  -- ex: int(1.5)
+
 
 # Converter float em inteiro :: float(<NumeroInteiro>) -- ex: float(1)
