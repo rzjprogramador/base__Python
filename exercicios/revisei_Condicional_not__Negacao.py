4a01f765890504eb268e2c19aa124688338c09aa
rzjprogramador <rzjprogramador@gmail.com>
2021-02-20 17:16:04 -0300

    Revisando Condicional Not Negacao

---
+++ exercicios/revisei_Condicional_not__Negacao.py
@@ -0,0 +1,23 @@
+num_1 = 0
+num_2 = 0
+ 
+if not num_1 != num_2:
+    print('Retorno 1')
+else:
+    print('Retorno 2')
+
+'''
+O código pode ser considerado um código mal escrito e com 
+complexidade desnecessária. 
+Sabendo disso, qual o retorno da expressão abaixo:
+
+ALERTA :
+Códigos devem resolver problemas simples de forma simples. No exemplo anterior, 
+nosso código tem dois valores iguais (0 e 0), porém, na nossa checagem estamos 
+usando "not" para inverter a checagem != (diferente). Nesse caso, o que seria falso 
+se torna verdadeiro e o que seria verdadeiro se torna falso. 
+Portanto, zero "NÃO" é "diferente" de zero.
+
+
+RESPOSTA CERTA :: Retorno 1
+'''
