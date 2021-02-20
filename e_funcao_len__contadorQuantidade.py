42941e3dff87bca3569730aef52b499ed793d6b4
rzjprogramador <rzjprogramador@gmail.com>
2021-02-20 17:58:07 -0300

    Contagem de caracteres de dados  enviados

---
+++ e_funcao_len__contadorQuantidade.py
@@ -0,0 +1,12 @@
+# função len -- contador de caracteres
+
+usuario = input('Digite seu nome: ')
+quant_caracteres = len(usuario)
+
+print(f'Seu nome é {usuario} e tem {quant_caracteres} caracteres.')
+
+# Analisar se digitou quantidade certa de caracteres
+if quant_caracteres < 6:
+    print('Você precisa digitar pelo menos 6 caracteres')
+else:
+    print('Cadastrado com sucesso')
---
+++ e_funcao_len_executando_contador_concatenado.py
@@ -0,0 +1,6 @@
+# Contagem de caracteres e execução de concatenação de dados enviados por input
+
+string_1 = input('Digite algo 1: ')
+string_2 = input('Digite algo 2: ')
+
+print(f'O total de caracteres digitados é {len(string_1) + len(string_2)} ')
