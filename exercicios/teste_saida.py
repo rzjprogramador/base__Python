3a2659ccc5fbc580546b9d5f682e2eb0174e3fc2
rzjprogramador <rzjprogramador@gmail.com>
2021-02-20 17:25:56 -0300

    teste saida

---
+++ exercicios/teste_saida.py
@@ -0,0 +1,17 @@
+# Qual a saída do código abaixo?
+
+if False:
+    print('Um')
+elif False:
+    print('Dois')
+elif False:
+    print('Três')
+elif True:
+    print('Quatro')
+elif False:
+    print('Cinco')
+else:
+    print('Seis')
+
+# Resposta == Quatro
+# Porque ele retorna o primeiro True que encontrar
