c236eec61ceb4b8b5b7153843add7dba0f4e5850
rzjprogramador <rzjprogramador@gmail.com>
2021-02-24 13:59:09 -0300

    Pass e ellipsi ... Blocos em espera

---
+++ pass__ellipsis__Blocos_em_Espera/pass__ellipsis.py
@@ -0,0 +1,22 @@
+# PASS E ELLIPSI... SÓ GUARDA O BLOCO PRA SER IMPLEMENTADO DEPOIS
+# sem erros : caso deixasse o bloco vazio dá erro
+# Pode ser usado tbm como placeholder o ellipsi 3 pontos...
+
+
+# PASS
+valor = False
+
+if valor:
+    pass
+else:
+    print('Tchau')
+
+
+# ELLIPSI ... MESMA COISA DO PASS -
+# SÓ GUARDA O BLOCO PRA SER IMPLEMENTADO DEPOIS
+valor2 = False
+
+if valor2:
+    print('Olá')
+else:
+    ...
