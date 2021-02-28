8200fded9a5d097b73e76e3314a16a8a682a2997
rzjprogramador <rzjprogramador@gmail.com>
2021-02-28 14:57:16 -0300

    Loops Conceito

---
+++ 7_loops/7-0__Loops__Conceito.py
@@ -0,0 +1,40 @@
+'''
+FOR == PARA
+Podemos interar(fazer um for um loop e separar por linha a linha e produzir de cada linha um item com indice e valor em qualquer sequencia
+(string - lista[] - tuplas()
+Menos boleano & em Um unico Numero Inteiro .
+
+'''
+
+# ------‐---------------------------------------------------------
+
+
+'''
+BREAK
+break ele interrompe o circulo ele mostra só a primeira volta antes do break e interrompe .
+
+'''
+
+# ------‐---------------------------------------------------------
+
+'''
+CONTINUE
+Continua acha o que pediu a instrucao antes..e continua o fluxo do loop 
+
+'''
+# ------‐---------------------------------------------------------
+
+
+'''
+ENUMERATE
+Numera mostra os indices de uma interacao
+- Vc adiciona o indice antes da var q vai guardar a interacao.
+- enumerate(NASEQUENCIA_ALVO)
+- e recupera os indices enumerados com este argumento na invocacao. ex:
+'''
+nome = 'Reinaldo'
+
+for i, letras in enumerate(nome):
+	print(i, letras)
+
+# ------‐---------------------------------------------------------
