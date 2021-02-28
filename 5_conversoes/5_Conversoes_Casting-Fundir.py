ae44428d4ebce24a79414e914cd19d3ef3340754
rzjprogramador <rzjprogramador@gmail.com>
2021-02-28 15:12:34 -0300

    Conversores

---
+++ 5_conversoes/5_Conversoes_Casting-Fundir.py
@@ -0,0 +1,19 @@
+nome = 'Reinaldo'
+
+# UPPER TRANSFORMA STRINGS EM MAIUSCULAS
+print(nome.upper())
+
+# LOWER TRANSFORMA STRINGS EM MINUSCULAS
+print(nome.lower())
+
+# ------‐---------------------------------------------------------
+'''
+- ACEITAR MAIUSCULA OU MINUSCULA -->> FUNCA0 LOWER() ::: 
+- VERIFICA PRIMEIRO VALOR DA SEQUENCIA -->> startswith('<ALVO>'):
+
+'''
+variavel_valor.lower() == 'm'  
+# ou
+variavel_valor.lower().startswith('m')
+
+# ------‐---------------------------------------------------------
--- 5_conversoes/5_Conversoes_String.py
+++
@@ -1,7 +0,0 @@
-nome = 'Reinaldo'
-
-# UPPER TRANSFORMA STRINGS EM MAIUSCULAS
-print(nome.upper())
-
-# LOWER TRANSFORMA STRINGS EM MINUSCULAS
-print(nome.lower())
