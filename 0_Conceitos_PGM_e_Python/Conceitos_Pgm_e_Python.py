c9112597c237f59fd5ee500db89f545a4ad012ac
rzjprogramador <rzjprogramador@gmail.com>
2021-02-28 15:23:55 -0300

    Conceitos PGM e Python

---
+++ 0_Conceitos_PGM_e_Python/Conceitos_Pgm_e_Python.py
@@ -0,0 +1,21 @@
+'''
+
+- Faz a Logica do que precisa
+
+- Faz  Validacao que do que vai receber é o tipo valido se nao for alerta o usuario.
+
+- Faz casting para converter a string para o que quiser receber na entrada de dados
+
+- Continua Complementando sua Logica.
+
+# ------‐---------------------------------------------------------
+No console averiguando valores caso resulyados forem Falso, nulos ou indefinidos 
+fazer algoritimo para corrigir e chegar ao esperado.
+.............................................................................
+
+
+
+
+
+
+'''
