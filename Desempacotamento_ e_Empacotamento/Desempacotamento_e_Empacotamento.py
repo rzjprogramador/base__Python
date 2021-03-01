077630e3d4102a158ce50ad0393f0156de4105ad
rzjprogramador <rzjprogramador@gmail.com>
2021-03-01 02:57:07 -0300

    Atribuindo lista a variaveis

--- Desempacotamento_ e_Empacotamento/Desempacotamento_e_Empacotamento.py
+++ Desempacotamento_ e_Empacotamento/Desempacotamento_e_Empacotamento.py
@@ -5,3 +5,11 @@ lista = [1, 2, 3, 4]
 print(*lista)
 # Mudando o separador do desempacotamento da lista
 print(*lista, sep='-')
+
+
+# Desempacotamento de listas e atribuindo a variaveis
+lista2 = ['Reinaldo', 'Gustavo', 'Leonardo', 1, 2, 3, 4, 5]
+
+n1, n2, n3, *demais = lista2
+
+print(n1, n2, n3, demais)
