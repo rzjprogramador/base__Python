2f80dd007fe51240034b9f2dfba0c85e57777d8d
rzjprogramador <rzjprogramador@gmail.com>
2021-03-01 02:41:46 -0300

    Metodo get

--- 0_Conceitos_PGM_e_Python/Conceitos_Pgm_e_Python.py
+++ 0_Conceitos_PGM_e_Python/Conceitos_Pgm_e_Python.py
@@ -13,9 +13,40 @@ No console averiguando valores caso resulyados forem Falso, nulos ou indefinidos
 fazer algoritimo para corrigir e chegar ao esperado.
 .............................................................................
 
+POSIÇÃO E VALOR ::
+Todas Sequencias : elementos,variaveis, atributos, objetos tem posicao e valor
+Posicao == indice/ i/ chave/
+Valor == valor da variavel
+.............................................................................
+Para acessar Posicao ou Valor de uma sequencia 
+podemos usar a função var.get('chave')
+
+O metodo get() é melhor que as [posicao] porque nos permite tratar erros
+caso a busca seja Nula ou None ex: 
+
+# idade nao existe nos meus args, entao tratar a busca pra nao dar erro
+    idade = kwargs.get('idade')
+
+    if idade is not None:
+        print(idade)
+    else:
+        print('Idade não existe.')
+    # Se quiser omitir o None só retirar o else
+    ---------------------------------------------
+
 
 
 
 
 
 '''
+lista = [1, 2, 3]
+pessoa = {'nome': 'Reinaldo', 'idade': 43, 'profissao': 'Prograamdor'}
+
+
+print(lista)
+print(lista[0])
+
+
+print(pessoa.get('nome'))
+
