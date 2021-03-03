ef87fc903d1d81e769d3c504caff7bc9ce407d47
rzjprogramador <rzjprogramador@gmail.com>
2021-03-03 15:04:05 -0300

    DESCOBRIR QUANTO AUMENTOU EM PORCENTAGEM

--- EXERCICIOS/porcentagem_1_.py
+++ EXERCICIOS/porcentagem_1_.py
@@ -4,11 +4,9 @@ COM VALOR FINAL JA DEFINIDO.
 
 '''
 
-
 peca1_nome = 'Cart'
 valor_inicial_peca1 = 400
 v_atual_peca1 = 450
-
 
 # Descobrindo quanto aumentou
 quanto_aumentou = v_atual_peca1 - valor_inicial_peca1
@@ -20,6 +18,7 @@ fracao_do_aumento = quanto_aumentou / valor_inicial_peca1
 
 # Descobrindo quanto em porcentagem aumentou
 porc_aumentou = fracao_do_aumento * 100
+
 print(f'''
 {peca1_nome} teve um aumento de {porc_aumentou}% 
 passando de {valor_inicial_peca1:.2f} para {v_atual_peca1:.2f}
