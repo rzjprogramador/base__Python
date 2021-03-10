feb458ce41b96695e09db758ee3f2a766da0368f
rzjprogramador <rzjprogramador@gmail.com>
2021-03-10 13:11:46 -0300

    Python

---
+++ 2--Atribuicoes.py
@@ -0,0 +1,11 @@
+# MULTIPLAS ATRIBUIÇÕES
+
+a, b, c = 10, 20, 30
+
+print(a)
+print(b)
+print(c)
+
+
+nome, sobrenome, profissao = 'Reinaldo', 'Zacharias', 'Programador'
+print(f'{nome} {sobrenome} {profissao}')
